ff30e7aea5e5a6e0638b3f807bac90e136d5c8ce
staaahli <florian.stallmach@gmail.com>
2025-05-06 00:01:08 +0200

    api is ass

--- rainbetbot.py
+++ rainbetbot.py
@@ -212,7 +212,7 @@ async def progress(interaction: discord.Interaction):
                 await interaction.response.send_message("❌ No milestones have been set by an admin.", ephemeral=True)
                 return
 
-        url = f"https://services.rainbet.com/v1/external/affiliates?start_at=2025-03-10&end_at={datetime.now().strftime('%Y-%m-%d')}&key={API_KEY}"
+        url = f"https://services.rainbet.com/v1/external/affiliates?start_at=2025-04-10&end_at={datetime.now().strftime('%Y-%m-%d')}&key={API_KEY}"
         response = requests.get(url)
         if response.status_code != 200:
             await interaction.response.send_message("❌ Failed to fetch data from the Rainbet API.", ephemeral=True)
@@ -315,7 +315,7 @@ async def link(interaction: discord.Interaction, rainbet: str, kick: str):
         return
 
     try:
-        url = f"https://services.rainbet.com/v1/external/affiliates?start_at=2025-03-10&end_at={datetime.now().strftime('%Y-%m-%d')}&key={API_KEY}"
+        url = f"https://services.rainbet.com/v1/external/affiliates?start_at=2025-04-10&end_at={datetime.now().strftime('%Y-%m-%d')}&key={API_KEY}"
         response = requests.get(url)
         if response.status_code != 200:
             await interaction.response.send_message("❌ Failed to fetch data from the Rainbet API.", ephemeral=True)
